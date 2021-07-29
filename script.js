b1dac417f98a705849659f7fa198ec481058d4e2
Pao <aungpao321@gmail.com>
2021-07-29 13:59:11 +0700

    test

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ const color = document.querySelector('#color')
 
 btn_toggle.onclick = () => {
   // your code here
+
 }
 
 // more codes for Search and Reset buttons here
+function highlight(text) {
+  var inputText = document.getElementById("length")
+  var innerHTML = inputText.innerHTML
+  var index = innerHTML.indexOf(text)
+  if (index >= 0) { 
+   innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length)
+   inputText.innerHTML = innerHTML
+  }
+}
+
+const sBtn = document.querySelector('#search')
+sBtn.onclick = () => {
+  highlight(length)
+}
